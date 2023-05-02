9ca53ddce0177122018a3da5ace090c40a8d51d1
bautibustos <47929345+bautibustos@users.noreply.github.com>
2023-05-02 20:53:05 -0300

    Manda el zip pero con un minuto de delay

--- tele.py
+++ tele.py
@@ -1,14 +1,14 @@
+from telegram import ReplyKeyboardMarkup, InlineKeyboardMarkup 
+from telegram.ext import Updater, CommandHandler, JobQueue, ContextTypes, CallbackContext
 import time
 import ConvertZip as Zip
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, JobQueue
 
 #mi id 5515915265
 
 with open("Utilidad\\token.txt", "r") as archivo:
     token = str(archivo.read())
 
-def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+def hello(update: Updater, context: ContextTypes.DEFAULT_TYPE) -> None:
     update.message.reply_text(f'Wuachin aca tene lo tusho {update.effective_user.first_name}')
     Zip.zipeo()
     with open("Utilidad\\name.txt", "r") as archivo:
@@ -17,11 +17,15 @@ def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     time.sleep(20)
 
 def main():
-    updater=Updater(token, use_context=True)#use_context=True
-    job_queue = updater.job_queue
-    updater.add_handler(CommandHandler("i", hello))
+    updater=Updater(token, update_queue=True)#use_context=True
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler('start', hello))
+    #updater.add_handler(CommandHandler('start',  hello))
     print("coshiendo")
     # Comienza el bot
     updater.start_polling()
     # Lo deja a la escucha. Evita que se detenga.
     updater.idle()
+
+main()
